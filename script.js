d5c5cfb26e0dcf2956861517fff15c846f7718c3
BRO-BHATT <525sunaybhatt@frhsd.com>
2024-01-22 09:32:09 -0500

    Started changing the JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
-let bill = 120 
+function calculateAndDisplay()
+{
+    //Get the input values from our HMTL forms
+    let bill = parseFloat(document.getElementById('totalBill').value)
+    let numOfPpl = parseInt(document.getElementById('numOfPeople').value)
+    let serviceQuality = document.getElementById('serviceQuality').value
+    
+    //Calculate tip, total bill, and amount per person using helper functions
+    let tip = calculateTip(bill, serviceQuality)
+    let totalBill = calculateTotalBill(bill, tip)
+    let amtPerPers = calculateAmtPerPers(totalBill, numOfPpl)
+
+
+    //Display anthe results in the HTML document
+    document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+    document.getElementById('totalBillResult').innerText
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let bill = 120 
 let numberOfPeople = 5
 let serviceQuality = 'Great'
 
@@ -34,3 +81,4 @@ let amtPerPers = calcAmtPerPrs(totalBill, numberOfPeople)
 console.log ('Tip: $' + tip)
 console.log('Total Bill: $' + totalBill)
 console.log('Amount per person: $' + amtPerPers)
+ */
